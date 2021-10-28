e677281998aaa2c379a3557fd4c265c33cccc943
Ubuntu <ubuntu@ip-172-31-23-137.us-east-2.compute.internal>
2021-10-28 16:42:54 +0000

    Updated SQL

--- customers.sql
+++ customers.sql
@@ -1,6 +1,19 @@
-select distinct b.customer_id, a.name, a.email, min(b.created_at) as first_order_at,
-                count(distinct b.id) as number_orders, max(b.created_at) as last_order_at
-from coffee_shop.customers a
-inner join coffee_shop.orders b on a.id = b.customer_id
-group by 1,2,3
-order by first_order_at limit 5
+with customer_orders as (
+	select
+		customer_id,
+		count(*) as number_of_orders,
+		min(created_at) as first_order_at
+	from coffee_shop.orders
+	group by 1
+)
+
+select
+	customers.id as customer_id,
+	customers.name,
+	customers.email,
+	customers_orders.first_order_at,
+	customer_orders.number_of_orders
+
+from coffee_shop.customers
+left join customer_orders
+	on customers.id = customer_orders.customer_id
